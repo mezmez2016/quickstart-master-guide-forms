01ac5472789dc00ad824fb9fa6f38b092b028d82
mezmez2016 <ezeouati@gmail.com>
2016-12-20 18:39:00 +0000

    Add files via upload

---
+++ hero.js
@@ -0,0 +1,15 @@
+"use strict";
+/**
+ * Created by m.ezeouati on 20/12/2016.
+ */
+var Hero = (function () {
+    function Hero(id, name, power, alterEgo) {
+        this.id = id;
+        this.name = name;
+        this.power = power;
+        this.alterEgo = alterEgo;
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
---
+++ hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by m.ezeouati on 20/12/2016.
+ */
+export class Hero {
+  constructor(public id: number, public name: string, public power: string, public alterEgo?: string) {
+  }
+}
